833929a8add83759e60a4f32b905e3717e16f0f2
TroyVerhelst <verhelsttroy@gmail.com>
2020-08-08 16:16:49 +0200

    Structure and file loading/saving

---
+++ src/main/java/com/mcmiddleearth/mcme/rpgtree/DBManager.java
@@ -0,0 +1,106 @@
+/*
+ * This file is part of MCME-pvp.
+ * 
+ * MCME-pvp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * MCME-pvp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MCME-pvp.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * 
+ */
+package com.mcmiddleearth.mcme.rpgtree;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DBManager {
+
+    @SuppressWarnings("unchecked")
+    public static void updateFile(Set<TreeType> treeTypes){
+        JSONArray treeTypesJSON = new JSONArray();
+        JSONObject treeTypeJSON = new JSONObject();
+        JSONArray treesJSON = new JSONArray();
+        JSONObject treeJSON = new JSONObject();
+        treeTypes.forEach(treeType -> {
+            treeTypeJSON.put("type", treeType.getType());
+            treeTypeJSON.put("maxGrowthStage", treeType.getMaxGrowthStage());
+            treeTypeJSON.put("growthMinutes", treeType.getGrowthMinutes());
+            treeType.getTrees().forEach(tree -> {
+                treeJSON.put("x", tree.getX());
+                treeJSON.put("y", tree.getY());
+                treeJSON.put("z", tree.getZ());
+                treeJSON.put("growthStage", tree.getGrowthStage());
+                treesJSON.add(treeJSON);
+            });
+            treeTypeJSON.put("trees", treesJSON);
+            treeTypesJSON.add(treeTypeJSON);
+        });
+
+        try (FileWriter file = new FileWriter(RPGTree.getPluginDirectory() + RPGTree.getFileSep() + "data.json")) {
+
+            file.write(treeTypesJSON.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static Set<TreeType> loadFile() throws Exception {
+        Set<TreeType> treeTypes = new HashSet<>();
+
+        JSONParser jsonParser = new JSONParser();
+        try (FileReader reader = new FileReader(RPGTree.getPluginDirectory() + RPGTree.getFileSep() + "data.json"))
+        {
+            JSONArray treeTypesJSON = (JSONArray) jsonParser.parse(reader);
+            treeTypesJSON.forEach(treeType -> {
+                JSONObject treeTypeJSON = (JSONObject) treeType;
+                TreeType tempTreeType = new TreeType();
+                tempTreeType.setType((String) treeTypeJSON.get("type"));
+                tempTreeType.setMaxGrowthStage((Integer) treeTypeJSON.get("maxGrowthStage"));
+                tempTreeType.setGrowthMinutes((Integer) treeTypeJSON.get("growthMinutes"));
+
+                JSONArray treesJSON = (JSONArray) treeTypeJSON.get("trees");
+                treesJSON.forEach(tree -> {
+                    JSONObject treeJSON = (JSONObject) tree;
+                    Tree tempTree = new Tree();
+                    tempTree.setGrowthStage((Integer) treeJSON.get("growthStage"));
+                    tempTree.setX((Integer) treeJSON.get("x"));
+                    tempTree.setY((Integer) treeJSON.get("y"));
+                    tempTree.setZ((Integer) treeJSON.get("z"));
+                    tempTreeType.getTrees().add(tempTree);
+
+                });
+                treeTypes.add(tempTreeType);
+            });
+
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException(e.getMessage());
+        } catch (IOException e) {
+            throw new IOException(e.getMessage());
+        } catch (ParseException e) {
+            throw new ParseException(e.getErrorType());
+        }
+
+        return treeTypes;
+    }
+
+}
--- src/main/java/com/mcmiddleearth/mcme/rpgtree/Tree.java
+++ src/main/java/com/mcmiddleearth/mcme/rpgtree/Tree.java
@@ -1,13 +1,41 @@
+/*
+ * This file is part of MCME-pvp.
+ *
+ * MCME-pvp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MCME-pvp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MCME-pvp.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
 package com.mcmiddleearth.mcme.rpgtree;
 
 import lombok.Getter;
 import lombok.Setter;
 
 public class Tree {
-    @Getter private Integer growthState;
-    @Getter @Setter private EventLocation treeLocation;
-    @Getter @Setter private Integer treeType;
-    public void grow(){
-        growthState ++;
+    @Getter
+    @Setter
+    private Integer growthStage;
+    @Getter
+    @Setter
+    private Integer x;
+    @Getter
+    @Setter
+    private Integer y;
+    @Getter
+    @Setter
+    private Integer z;
+
+    public void increaseGrowthStage() {
+        growthStage++;
     }
 }
